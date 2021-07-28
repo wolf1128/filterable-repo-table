b8efbaed32290af1e23cfc8ff1763ef13712bd1f
Professor.dev <professor.dev.run@gmail.com>
2021-07-28 16:59:33 +0430

    Built a static version

--- src/components/FilterableRepositoryTable.js
+++ src/components/FilterableRepositoryTable.js
@@ -4,22 +4,53 @@ import Tabs from './Tabs';
 import RepositoryTable from './RepositoryTable';
 
 const FilterableRepositoryTable = () => {
-    return (
-        <>
-            <nav className="panel">
-                <p className="panel-heading">
-                    Repositories
-                </p>
-                <div className="panel-block">
-                    <SearchBar />
-                </div>
-                <p className="panel-tabs">
-                    <Tabs />
-                </p>
-                <RepositoryTable />
-            </nav>            
-        </>
-    )
-}
+	return (
+		<>
+			<nav className='panel'>
+				<p className='panel-heading'>Repositories</p>
+				<div className='panel-block'>
+					<SearchBar />
+				</div>
+				<p className='panel-tabs'>
+					<Tabs />
+				</p>
+				<RepositoryTable
+					repositories={[
+						{
+							name: 'bulma',							
+							private: false, // public, private
+							fork: false, // fork/source
+						},
+						{
+							name: 'marksheet',							
+							private: false,
+							fork: false,
+						},
+						{
+							name: 'minireset.css',							
+							private: false,
+							fork: false,
+						},
+						{
+							name: 'jgthms.github.io',							
+							private: false,
+							fork: false,
+						},
+						{
+							name: 'daniellowtw/infboard',							
+							private: false,
+							fork: true,
+						},
+						{
+							name: 'mojs',							
+							private: false,
+							fork: true,
+						},
+					]}
+				/>
+			</nav>
+		</>
+	);
+};
 
-export default FilterableRepositoryTable
+export default FilterableRepositoryTable;
--- src/components/RepositoryTable.js
+++ src/components/RepositoryTable.js
@@ -1,43 +1,43 @@
 import React from 'react'
 
-const RepositoryTable = () => {
+const RepositoryTable = ({ repositories }) => {
     return (
         <>
             <a className="panel-block is-active">
                 <span className="panel-icon">
                     <i className="fas fa-book" aria-hidden="true"></i>
                 </span>
-                bulma
+                {repositories[0].name}
             </a>
             <a className="panel-block">
                 <span className="panel-icon">
                     <i className="fas fa-book" aria-hidden="true"></i>
                 </span>
-                marksheet
+                {repositories[1].name}
             </a>
             <a className="panel-block">
                 <span className="panel-icon">
                     <i className="fas fa-book" aria-hidden="true"></i>
                 </span>
-                minireset.css
+                {repositories[2].name}
             </a>
             <a className="panel-block">
                 <span className="panel-icon">
                     <i className="fas fa-book" aria-hidden="true"></i>
                 </span>
-                jgthms.github.io
+                {repositories[3].name}
             </a>
             <a className="panel-block">
                 <span className="panel-icon">
                     <i className="fas fa-code-branch" aria-hidden="true"></i>
                 </span>
-                daniellowtw/infboard
+                {repositories[4].name}
             </a>
             <a className="panel-block">
                 <span className="panel-icon">
                     <i className="fas fa-code-branch" aria-hidden="true"></i>
                 </span>
-                mojs
+                {repositories[5].name}
             </a>
             <div className="panel-block">
                     <button className="button is-link is-outlined is-fullwidth">
